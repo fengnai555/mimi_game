2846dfc92c78cb85fe5c4e9d020dc4a7eb005c48
fengnai555 <fengnai555@example.com>
2026-02-27 13:26:18 +0800

    feat: update pyramid structure and dev server configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // @ts-ignore
+  allowedDevOrigins: ['mimi.nmco.dpdns.org', 'nmco.dpdns.org', '122.117.166.16'],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['mimi.nmco.dpdns.org', 'nmco.dpdns.org', '122.117.166.16']
+    }
+  }
 };
 
 export default nextConfig;
